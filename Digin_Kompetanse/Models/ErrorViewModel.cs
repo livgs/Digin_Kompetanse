caf89cf8f857586d5d87161e98ad466f92d7d4df
livgs <livgs@uia.no>
2025-08-19 12:02:00 +0200

    Initial commit

---
+++ Digin_Kompetanse/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Digin_Kompetanse.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
